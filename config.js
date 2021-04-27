703fbc318c55615859e210b94367bc9c22808290
Hayao3001 <programinghayao@gmail.com>
2021-04-27 15:37:15 +0900

    index以外削除

--- config.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDpJCmeoX_y8uPscoT3FXu5RJHvrMKPBAI",
-    authDomain: "goods-management-tool.firebaseapp.com",
-    projectId: "goods-management-tool",
-    storageBucket: "goods-management-tool.appspot.com",
-    messagingSenderId: "510122414702",
-    appId: "1:510122414702:web:50b842c15cd4ba4a67cc7f",
-    measurementId: "G-3XRQZ3K33K"
-  };
